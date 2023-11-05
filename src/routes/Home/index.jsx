628ee0409d01dd39e132dc1eac03f930c88552fe
github.com/nicolyy <nicollyoliveira003@gmail.com>
2023-11-05 14:31:39 -0300

    Adicionando conteudo

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -37,13 +37,8 @@ export default function Home() {
         <div className="sobre-mim-box">
             <img className="sobre-mim-img " src= {Allan} alt="Imagem do profissional Allan Kelwin"/>
             <h2 className="sobre-mim-titulo">Sobre Mim</h2>
-            <p className="sobre-mim-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                 industry's standard dummy text ever since the 1500s, when an unknown printer took 
-                 a galley of type and scrambled it to make a type specimen book. It has survived not 
-                 only five centuries, but also the leap is simply dummy text of the printing and 
-                 typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever 
-                 since the 1500s, when an unknown printer took a galley of type and scrambled it to 
-                 make a type speci</p>
+            <p className="sobre-mim-texto">Profissional proativo e dedicado, com características de escuta ativa e comunicação elogiadas, 
+            sempre em busca de melhorias em seu ambiente de trabalho, aprimorando processos diários ou propondo novos projetos.</p>
         </div>
         <div className="curriculo-box">
             <details className="curriculo opcao1">
@@ -51,9 +46,19 @@ export default function Home() {
                     <img src={Seta} alt="Seta para abrir"/>
                 </summary>
                 <div className="summary-open">
-                    <p className="summary-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took 
-                        a galley of type and scrambled it to make a type specimen book</p>
+                    <ul className="summary-texto">
+                        <li>
+                            <h3>Educação Fisica (Licenciatura)</h3>
+                            <p>Centro Universitário das Faculdades Metropolitanas Unidas - FMU</p>
+                            <p>Janeiro de 2020 - Dezembro de 2023</p>   
+                        </li>
+                        <li>
+                            <h3>Tecnico em Adminstração</h3>
+                            <p>Etec de Sapopemba</p>
+                            <p>Janeiro de 2017 - Dezembro de 2019</p>
+
+                        </li>
+                    </ul>
                 </div>
             </details>
 
@@ -62,9 +67,13 @@ export default function Home() {
                    <img src={Seta} alt="Seta para abrir"/>
                 </summary>
                 <div className="summary-open">
-                    <p className="summary-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took 
-                        a galley of type and scrambled it to make a type specimen book</p>
+                    <ul className="summary-texto">
+                        <li>
+                            <p>Inglês - <strong>Avançado</strong></p>
+                        </li>
+                        <li>
+                            <p>Português - <strong>Avançado</strong></p> </li>
+                    </ul>
                 </div>
             </details>
             <details className="curriculo opcao3">
@@ -72,9 +81,28 @@ export default function Home() {
                    <img src={Seta} alt="Seta para abrir"/>
                 </summary>
                 <div className="summary-open">
-                    <p className="summary-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took 
-                        a galley of type and scrambled it to make a type specimen book</p>
+                    <ul className="summary-texto">
+                        <li>
+                            <h3>Colégio Palmares</h3>
+                            <p>Estágiario</p>
+                            <p>Setembro de 2022 - Até o momento</p>
+                        </li>
+                        <li>
+                            <h3>Colégio Brasil Canáda</h3>
+                            <p>Professor Especialista</p>
+                            <p>Junho de 2023 - Até o momento</p>
+                        </li>
+                        <li>
+                            <h3>Saint Nicholas School</h3>
+                            <p>Professor Especialista</p>
+                            <p>Setembro de 2023 - Até o momento</p>
+                        </li>
+                        <li>
+                            <h3>Corpo Brinquedo LTDA</h3>
+                            <p>Coordenador</p>
+                            <p>Junho de 2023 - Até o momento</p>
+                        </li>
+                    </ul>
                 </div>
             </details>
             <details className="curriculo opcao4">
@@ -82,9 +110,13 @@ export default function Home() {
                    <img src={Seta} alt="Seta para abrir"/>
                 </summary>
                 <div className="summary-open">
-                    <p className="summary-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the
-                        industry's standard dummy text ever since the 1500s, when an unknown printer took 
-                        a galley of type and scrambled it to make a type specimen book</p>
+                    <ul className="summary-texto">
+                        <li>Planejamento de Aulas</li>
+                        <li>Desenvolvimento e acompanhamento de habilidades física dos alunos</li>
+                        <li>Elaboraçaõ de projetos</li>
+                        <li>Preparação de Eventos</li>
+                        <li>Pacote Office - Avançado </li>
+                    </ul>
                 </div>
             </details>
             </div>
@@ -93,16 +125,14 @@ export default function Home() {
      <section >
         <div className="professor-alunos"> 
             <div className="colum1-professor">
-                <h1 className="professor-titulo">Alguma coisa Aqui</h1>
-                <p className="professor-texto">is simply dummy text of the printing and typesetting industry. 
-                    Lorem Ipsum has been the industry's standard dummy text ever 
-                    since the 1500s, when an unknown printer took a galley of type
-                     and scrambled it to make a type specimen book. It has survived 
-                     not only five centuries, but also the leap is simply dummy text 
-                     of the printing and typesetting industry. Lorem Ipsum has been 
-                     the industry's standard dummy text ever since the 1500s, 
-                     when an unknown printer took a galley of type and scrambled it
-                      to make a type speci</p>
+                <h1 className="professor-titulo">O Professor</h1>
+                <p className="professor-texto">É um profissional dedicado ao ensino de atividades
+                 de carater motor, incluindo esportes, jogos e brincadeiras, danças e lutas. 
+                 Ele projeta e ministra aulas, promove o desenvolvimento da coordenação,
+                  possibilita momentos de evolução socioemocional entre os alunos, 
+                  além de incentivar hábitos saudáveis de vida e a importância do exercício físico. 
+                  É responsável por criar projetos de treinamento, eventos, competitivos ou não,
+                   avaliar o progresso dos alunos e fomentar a importância da atividade física para o bem-estar geral.</p>
             </div>
             <div className="colum2-professor">
 
@@ -118,14 +148,16 @@ export default function Home() {
 
       <section className="projetos">
           <article className="projeto-right linha1">
-              <div className="conteudo-projeto">
-                  <h2 className="projeto-titulo">Titulo do projeto aqui</h2>
-                  <p className="projeto-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                      standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-                      and scrambled it to make a type specimen book. It has survived not only five centuries, 
-                      but also the leap is simply dummy text of the printing and typesetting industry. Lorem 
-                      Ipsum has been the industry's standard dummy text ever since the 1500s, ]
-                      when an unknown printer took a gall</p>
+              <div className="conteudo-projeto-linha1">
+                  <h2 className="projeto-titulo">Cheerleading</h2>
+                  <p className="projeto-texto">A modalidade esportiva do cheerleading alinhada 
+                    ao planejamento escolar, pode trazer benefícios variados e notáveis no processo
+                    pedagógico dos alunos praticantes. Entrando no mérito fisiológico, as vantagens
+                    acerca de consciência corporal, flexibilidade e força são as mais acentuadas 
+                    conforme a criança ou adolescente começa a praticar o esporte com uma boa frequência.
+                     Os movimentos novos promovem estímulos cognitivos e motores que são em sua grande
+                     maioria, novos para aquele aluno, que se vê descobrindo um universo de possibilidades
+                    longe do eixo tradicional dos esportes mais conhecidos.</p>
               </div>
               <figure>
               <img className="projeto-img" src={Img1} alt=""/>
@@ -133,13 +165,12 @@ export default function Home() {
           </article>
           <article className="projeto-left linha2">
               <div className="conteudo-projeto">
-                  <h2 className="projeto-titulo">Titulo do projeto aqui</h2>
-                  <p className="projeto-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                      standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-                      and scrambled it to make a type specimen book. It has survived not only five centuries, 
-                      but also the leap is simply dummy text of the printing and typesetting industry. Lorem 
-                      Ipsum has been the industry's standard dummy text ever since the 1500s, ]
-                      when an unknown printer took a gall</p>
+                  <h2 className="projeto-titulo">Circo</h2>
+                  <p className="projeto-texto-left">As propostas circenses dentro do âmbito escolar representam 
+                  um estímulo alternativo muito válido para o desenvolvimento socioemocional e motor 
+                  dos alunos. A presença da característica artística é um ponto de  
+                  interdisciplinaridade que pode ser muito aproveitado com o devido planejamento, 
+                  possibilitando frutos positivos para os alunos.</p>
               </div>
               <figure>
               <img className="projeto-img" src={Img2} alt=""/>
@@ -147,13 +178,14 @@ export default function Home() {
           </article>
           <article className="projeto-right linha3">
               <div className="conteudo-projeto">
-                  <h2 className="projeto-titulo">Titulo do projeto aqui</h2>
-                  <p className="projeto-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                      standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-                      and scrambled it to make a type specimen book. It has survived not only five centuries, 
-                      but also the leap is simply dummy text of the printing and typesetting industry. Lorem 
-                      Ipsum has been the industry's standard dummy text ever since the 1500s, ]
-                      when an unknown printer took a gall</p>
+                  <h2 className="projeto-titulo">Vivências Esportivas</h2>
+                  <p className="projeto-texto">Aulas de iniciação aos esportes coletivos,
+                   são imprescindíveis para a formação integral do aluno de até 6 anos. 
+                   Promovendo estas atividades, os benefícios de interação social, 
+                   habilidade motora fina e execução de técnicas específicas 
+                   são claros quando o aluno atinge os anos iniciais do ensino 
+                   fundamental e consegue desempenhar atividades cotidianas com 
+                   maior autonomia devido ao ganho de repertório motor prévio.</p>
               </div>
               <figure>
               <img className="projeto-img" src={Img3} alt=""/>
@@ -161,13 +193,13 @@ export default function Home() {
           </article>
           <article className="projeto-left linha4">
               <div className="conteudo-projeto">
-                  <h2 className="projeto-titulo">Titulo do projeto aqui</h2>
-                  <p className="projeto-texto">is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's 
-                      standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
-                      and scrambled it to make a type specimen book. It has survived not only five centuries, 
-                      but also the leap is simply dummy text of the printing and typesetting industry. Lorem 
-                      Ipsum has been the industry's standard dummy text ever since the 1500s, ]
-                      when an unknown printer took a gall</p>
+                  <h2 className="projeto-titulo">Trabalho Social</h2>
+                  <p className="projeto-texto-left">Dentre todas as possibilidades pedagógicas no 
+                  âmbito da educação física, se mostra cada vez mais presente a desigualdade
+                   social e a diferença de oportunidades de acordo com o nível de recursos
+                    monetários de cada família. Possibilitar vivências e oficinas com foco 
+                    no público mais carente, é uma resposabilidade para os professores que
+                     procuram fazer a diferença para os alunos que mais precisam.</p>
               </div>
               <figure>
               <img className="projeto-img" src={Img4} alt=""/>
